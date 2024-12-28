976810f9f079ffeec693993d7c47cc8490f07b7f
Kalai <kalaisanu@gmail.com>
2024-12-28 15:27:36 +0530

    Completed playground Challenge

---
+++ src/Patient.java
@@ -0,0 +1,99 @@
+class Patient {
+    private String name;
+    private int age;
+    private String gender;
+    private String symptoms;
+    private String treatment;
+    private String appointmentDay;
+
+    // Constructor
+    public Patient(String name, int age, String gender, String symptoms) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.symptoms = symptoms;
+        this.treatment = null;
+        this.appointmentDay = null;
+    }
+
+    // Getter methods for name, age, gender, symptoms, treatment, appointmentDay
+    // TODO
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAppointmentDay() {
+        return appointmentDay;
+    }
+
+    public void setAppointmentDay(String appointmentDay) {
+        this.appointmentDay = appointmentDay;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    // Setter methods for treatment, appointmentDay
+    // TODO
+
+    public String getSymptoms() {
+        return symptoms;
+    }
+
+    public void setSymptoms(String symptoms) {
+        this.symptoms = symptoms;
+    }
+
+    public String getTreatment() {
+        return treatment;
+    }
+
+    public void setTreatment(String treatment) {
+        this.treatment = treatment;
+    }
+
+    /* The below equals() and hashCode() methods are crucial for the proper functioning of
+       collections like HashSet, HashMap, and other hash-based collections. */
+
+    // Override equals() to compare Patient objects based on name, age, and gender
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Patient patient = (Patient) obj;
+        return age == patient.age && name.equals(patient.name) && gender.equals(patient.gender);
+    }
+
+    // Override hashCode() to generate hash code based on name, age, and gender
+    @Override
+    public int hashCode() {
+        return name.hashCode() + age + gender.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{name='" + name + "', age=" + age + ", gender=" + gender + "}";
+    }
+}
